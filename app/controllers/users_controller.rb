41247fdcb1e098218e5419084916453fb55cf1ec
tamnd-1034 <nguyen.duy.tam@sun-asterisk.com>
2020-07-29 10:37:42 +0700

    Chapter 7: Sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      redirect_to root_path
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".new.success_notify"
+      redirect_to @user
+    else
+      flash[:danger] = t ".new.faild_notify"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::USERS_PARAMS
+  end
 end
